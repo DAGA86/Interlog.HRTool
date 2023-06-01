d37e82865ecbc4209117fd2add7143e233beac53
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-06-01 15:23:26 +0100

    Create BaseController with context

---
+++ Interlog.HRTool.WebApp/Controllers/_BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Interlog.HRTool.Data.Contexts;
+using Interlog.HRTool.Data.Providers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interlog.HRTool.WebApp.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public BaseController(DatabaseContext context)
+        {
+            _context = context;
+        }
+    }
+}

d3baaced8e900748fd8ed7a144277358d67cfe5d
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-06-05 16:47:37 +0100

    added stringLength in property name

--- Interlog.HRTool.WebApp/Models/Company/CompanyViewModel.cs
+++ Interlog.HRTool.WebApp/Models/Company/CompanyViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Interlog.HRTool.WebApp.Models.Company
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interlog.HRTool.WebApp.Models.Company
 {
     public class CompanyViewModel
     {
         public int Id { get; set; }
+        [StringLength(64)]
         public string Name { get; set; }
 
     }
